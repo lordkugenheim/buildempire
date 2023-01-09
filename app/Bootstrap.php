4112c02a89933390dd4ef257c47be1e4a8eb90b1
LK002\Ben <ben@ben-taylor.co.uk>
2023-01-09 14:10:14 +0000

    add npm lockfile add vlucas/dotenv to composer load .env into project in bootstrap.php add default .env file

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -1,11 +1,30 @@
 <?php
 
+require "../vendor/autoload.php";
+
 // load any php file inside the /config folder
 foreach (scandir('../config') as $filename) {
     if (strpos($filename, 'php') !== false) {
         require_once '../config/' . $filename;
     }
 }
+
+$dotenv = Dotenv\Dotenv::createImmutable(DIR_ROOT);
+$dotenv->load();
+
+$capsule = new Illuminate\Database\Capsule\Manager;
+
+$capsule->addConnection([
+   "driver"   => env('DB_DRIVER'),
+   "host"     => env('DB_HOST'),
+   "database" => env('DB_DATABASE'),
+   "username" => env('DB_USERNAME'),
+   "password" => env('DB_PASSWORD')
+]);
+
+$capsule->setAsGlobal();
+
+$capsule->bootEloquent();
 
 // Autoloader for core App, Controller and Model classes
 spl_autoload_register(function ($class) {
